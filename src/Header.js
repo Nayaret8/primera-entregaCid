f89b2098805b11d22b83cba322e56b61af7390a1
Nayaret Cid <navicequi@gmail.com>
2022-03-21 19:40:11 -0300

    Agregando el nav

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavBar from "./componentes/NavBar";
+import './Header.css';
+
+
+const Header = () => {
+
+    return (
+        <>
+        <h1>Bienvenidos a nuestra tienda</h1>
+        <div>
+            <NavBar/>
+        </div>
+
+        </>
+    )
+} 
+
+export default Header
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './NavBar.css';
+
+const NavBar = () => {
+
+    return (
+        <nav>
+            <a href="#">Contenido 1</a>
+            <a href="#">Contenido 2</a>
+            <a href="#">Contenido 3</a>
+            <a href="#">Contenido 4</a>
+
+        </nav>
+    )
+}
+
+export default NavBar
